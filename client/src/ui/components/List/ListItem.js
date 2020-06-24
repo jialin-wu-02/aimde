0211118b6ad8eacf6dd08243f398831df045a2b0
jialin-wu-02 <jialin.wu.02@gmail.com>
2020-06-24 16:23:43 -0700

    update listitem to be hoverable

--- client/src/ui/components/List/ListItem.js
+++ client/src/ui/components/List/ListItem.js
@@ -1,19 +1,33 @@
 import './List.less';
 
-import React  from 'react';
+import React , { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { classNames } from '../../utils';
 
 
-function ListItem({ children, className, description }) {
+function ListItem({ children, className, description, onHoverEffect }) {
   const compClassName = classNames({
     ListItem: true,
     [className]: !!className,
   });
 
+  const [hoverStyle, setHoverStyle] = useState(null)
+
+  const onMouseEnterHandler = () => {
+    if (onHoverEffect) {
+      setHoverStyle({
+        paddingLeft: '0.5em',
+        backgroundColor: '#f6f6f6',
+      });
+    }
+  }
+  const onMouseLeaveHandler = () => {
+    setHoverStyle(null);
+  }
+
   return (
-    <li className={compClassName}>
+    <li className={compClassName} onMouseEnter={onMouseEnterHandler} onMouseLeave={onMouseLeaveHandler} style={hoverStyle}>
       {children}
       {!!description &&
         <div className='ListItem__desc'>{description}</div>
